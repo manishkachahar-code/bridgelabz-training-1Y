541d32dc3195b8dd16e5195c094f66d62a66fa11
manishkachahar-code <manishkachahar@gmail.com>
2026-03-13 15:36:06 +0530

    Added StringToLowerCaseExample in Workshop branch

---
+++ StringToLowerCaseExample.java
@@ -0,0 +1,8 @@
+public class StringToLowerCaseExample{
+public static void main(String[]args){
+string originalString = "HeLLO WorLD! 123";
+string lowerCaseString = originalString.toLowerCase();
+System.out.println("Original String: " + originalString);
+System.out.println("Lowercase String: " +lowerCaseString);
+}
+}
