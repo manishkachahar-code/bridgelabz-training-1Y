e2f61184a0adf1ecf22a1a4cee1a4224980316ca
manishkachahar-code <manishkachahar@gmail.com>
2026-03-17 12:49:23 +0530

    Added Java codes

---
+++ Interfaces Abstraction/src/com/gla/Abstraction/PasswordCheck.java
@@ -0,0 +1,12 @@
+package com.gla.Abstraction;
+interface SecurityUtils {
+    static boolean isStrong(String password) {
+        return password.length() >= 8;
+    }
+}
+
+public class PasswordCheck {
+    public static void main(String[] args) {
+        System.out.println(SecurityUtils.isStrong("Password123"));
+    }
+}
---
+++ Interfaces Abstraction/src/com/gla/Abstraction/StringChecker.java
@@ -0,0 +1,10 @@
+package com.gla.Abstraction;
+import java.util.function.Function;
+
+public class StringChecker {
+}
+public static void main(String[] args) {
+    Function<String, Integer> length = str -> str.length();
+    System.out.println(length.apply("Hello"));
+}
+
