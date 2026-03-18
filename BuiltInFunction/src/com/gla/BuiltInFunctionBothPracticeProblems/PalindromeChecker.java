0cfd45ee677f597440f834645c9fbaccb7911096
manishkachahar-code <manishkachahar@gmail.com>
2026-03-18 11:08:44 +0530

    Added BuiltInFunction in Methods branch

---
+++ BuiltInFunction/src/com/gla/BuiltInFunctionBothPracticeProblems/PalindromeChecker.java
@@ -0,0 +1,30 @@
+package com.gla.BuiltInFunctionBothPracticeProblems;
+import java.util.Scanner;
+public class PalindromeChecker {
+    static String getInput(Scanner sc) {
+        System.out.print("Enter a string: ");
+        return sc.nextLine();
+    }
+
+    static boolean isPalindrome(String str) {
+        String reversed = new StringBuilder(str).reverse().toString();
+        return str.equalsIgnoreCase(reversed);
+    }
+
+    static void displayResult(boolean result) {
+        if (result)
+            System.out.println("It is a palindrome.");
+        else
+            System.out.println("It is not a palindrome.");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = getInput(sc);
+        boolean result = isPalindrome(input);
+        displayResult(result);
+        sc.close();
+    }
+
+
+}
