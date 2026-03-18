90aac6d2affc8731eb0f00e11e09fe7bdd42783d
manishkachahar-code <manishkachahar@gmail.com>
2026-03-18 10:49:22 +0530

    Added ExtrasJavaString in string-api branch

---
+++ ExtrasJavaString/src/com/gla/ExtrasJavaString/RemoveChar.java
@@ -0,0 +1,20 @@
+package com.gla.ExtrasJavaString;
+
+public class RemoveChar {
+    public static void main(String[] args) {
+
+        String str = "Hello World";
+        char remove = 'l';
+
+        String result="";
+
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) != remove){
+                result += str.charAt(i);
+            }
+        }
+
+        System.out.println("Modified String: "+result);
+    }
+
+}
---
+++ ExtrasJavaString/src/com/gla/ExtrasJavaString/ToogleCase.java
@@ -0,0 +1,22 @@
+package com.gla.ExtrasJavaString;
+import java.util.Scanner;
+public class ToogleCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        String result="";
+
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+
+            if(Character.isUpperCase(ch))
+                result += Character.toLowerCase(ch);
+            else
+                result += Character.toUpperCase(ch);
+        }
+
+        System.out.println("Toggle Case: "+result);
+    }
+
+}
