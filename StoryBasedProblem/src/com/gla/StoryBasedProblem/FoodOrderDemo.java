320897d80595f971dc0b39700a5ee50da228dcd9
manishkachahar-code <manishkachahar@gmail.com>
2026-03-23 15:38:29 +0530

    Added StoryBasedProblem in Workshop branch

---
+++ StoryBasedProblem/src/com/gla/StoryBasedProblem/FoodOrderDemo.java
@@ -0,0 +1,54 @@
+package com.gla.StoryBasedProblem;
+abstract class Order {
+    protected int orderId;
+    protected double baseAmount;
+
+    public Order(int orderId, double baseAmount) {
+        this.orderId = orderId;
+        this.baseAmount = baseAmount;
+    }
+
+
+    public abstract double calculateBill();
+
+    public static final double deliveryCharge = 40.0;
+}
+
+
+class RegularOrder extends Order {
+    public RegularOrder(int orderId, double baseAmount) {
+        super(orderId, baseAmount);
+    }
+
+    @Override
+    public double calculateBill() {
+        return baseAmount + deliveryCharge;
+    }
+}
+
+class PremiumOrder extends Order {
+    public PremiumOrder(int orderId, double baseAmount) {
+        super(orderId, baseAmount);
+    }
+
+    @Override
+    public double calculateBill() {
+        double discounted = baseAmount * 0.8;  // 20% off
+        return discounted + deliveryCharge;
+    }
+}
+public class FoodOrderDemo {
+    public static void main(String[] args) {
+        // Polymorphic array
+        Order[] orders = {
+                new RegularOrder(101, 200.0),
+                new PremiumOrder(102, 300.0),
+                new RegularOrder(103, 150.0)
+        };
+
+        for (Order order : orders) {
+            System.out.println("Order " + order.orderId + " Bill: $" +
+                    String.format("%.2f", order.calculateBill()));
+        }
+    }
+}
